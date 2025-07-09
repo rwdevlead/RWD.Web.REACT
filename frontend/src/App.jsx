ed6927f6a076b7655238ffefd051066e91469092
Jim Stevens <realworlddevs@outlook.com>
2025-07-08 21:59:54 -0400

    navbar stubbed out

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,13 @@
 import { useState } from "react";
 import "./App.css";
 import { LoadingScreen } from "./components/LoadingScreen";
+import { Navbar } from "./components/NavBar";
+import { MobileMenu } from "./components/MobileMenu";
 import "./index.css";
 
 function App() {
   const [isLoaded, setIsLoaded] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
 
   return (
     <>
@@ -13,7 +16,10 @@ function App() {
         className={`min-h-screen transform-opacity duration-700 ${
           isLoaded ? "opacity-100" : "opacity-0"
         } bg-black text-gray-100`}
-      ></div>
+      >
+        <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+        <MobileMenu menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+      </div>
     </>
   );
 }
