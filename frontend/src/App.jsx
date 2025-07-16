25aee8c9ded7afc26d2d7c8cf9e2cff557dc8d90
Jim Stevens <realworlddevs@outlook.com>
2025-07-15 20:24:10 -0400

    about page stubbed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,8 @@ import "./App.css";
 import { LoadingScreen } from "./components/LoadingScreen";
 import { Navbar } from "./components/NavBar";
 import { MobileMenu } from "./components/MobileMenu";
+import { Home } from "./components/sections/Home";
+import { About } from "./components/sections/About";
 import "./index.css";
 
 function App() {
@@ -19,6 +21,8 @@ function App() {
       >
         <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
         <MobileMenu menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+        <Home />
+        <About />
       </div>
     </>
   );
