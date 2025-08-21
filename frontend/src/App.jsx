e7c4c2fcf5706b6782cd86d1c591e87fb7209d24
Jim Stevens <realworlddevs@outlook.com>
2025-08-21 18:48:11 -0400

    prebuild for preview deploy - fix case sensitive reference

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import "./App.css";
 import { LoadingScreen } from "./components/LoadingScreen";
-import { Navbar } from "./components/NavBar";
+import { Navbar } from "./components/Navbar";
 import { MobileMenu } from "./components/MobileMenu";
 import { Home } from "./components/sections/Home";
 import { About } from "./components/sections/About";
